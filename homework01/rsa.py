cea826bc8d5949329fca43a95caf2c7754a4402a
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-10-05 00:35:14 +0500

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -63,10 +63,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    n = p*q
+    n = p * q
 
 
-    phi = (p-1)*(q-1)
+    phi = (p-1) * (q-1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
