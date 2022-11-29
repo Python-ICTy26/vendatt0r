7b135aa735357c6437c83ba4e01057ee61b039e0
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 19:05:03 +0500

    Update wall.py

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -2,12 +2,14 @@ import textwrap
 import time
 import typing as tp
 from string import Template
-
+from math import ceil
+from time import sleep
 import pandas as pd
 from pandas import json_normalize
 
-from vkapi import config, session
-from vkapi.exceptions import APIError
+from homework05.vkapi.config import VK_CONFIG
+from homework05.vkapi.session import Session
+from homework05.vkapi.exceptions import APIError
 
 
 def get_posts_2500(
@@ -20,7 +22,7 @@ def get_posts_2500(
     extended: int = 0,
     fields: tp.Optional[tp.List[str]] = None,
 ) -> tp.Dict[str, tp.Any]:
-    pass
+    return {}
 
 
 def get_wall_execute(
@@ -49,4 +51,32 @@ def get_wall_execute(
     :param fields: Список дополнительных полей для профилей и сообществ, которые необходимо вернуть.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+    current_session = Session(base_url=VK_CONFIG["domain"])
+
+    code = """return API.wall.get({
+        "owner_id": "%s",
+        "domain": "%s",
+        "offset": %d,
+        "count": "%d",
+        "filter": "%s",
+        "extended": %d,
+        "fields": "%s",
+        "v": "%s"
+    });"""
+    answer = []
+    for i in range(ceil(count / 100)):
+        code_for_request = code % (
+            owner_id, domain, 100 * i, 100 * (i + 1) if count > 100 else count, filter, extended, fields, VK_CONFIG["version"]
+        )
+        response = current_session.post(
+            url="execute",
+            data={
+                "code": code_for_request,
+                "access_token": VK_CONFIG["access_token"],
+                "v": VK_CONFIG["version"]
+            }
+        )
+        answer += response.json()["response"]["items"]
+        if i % 2 == 0:
+            sleep(1)
+    return json_normalize(answer)
