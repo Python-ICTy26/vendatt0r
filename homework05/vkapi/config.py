4657efaf77b04ffc56521878287723f3fddf9583
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 21:37:29 +0500

    Update config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -3,5 +3,5 @@
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
     "access_token": "vk1.a.BUioWFAl5digIgXZnD4c5mtw2bWkjHXu2C2rhWpik48Wv6Fdc_tqViCkdUUrFGN8h_XxXuuCD7q_BalFrpiG_PeI1z-D2ud5V9-UdWoW0kTsx7XXzSnMPATdTV24CmhOqB7Xi0qk6-LW2ZlEN202X6p9kMzLxN-d-R4rE4pdtgCocVJzchn-AvC-GT0sfcQM",
-    "version": "5.126",
+    "version": "5.131",
 }
