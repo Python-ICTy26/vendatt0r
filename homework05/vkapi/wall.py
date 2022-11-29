4d7583d822efc610e243b0aaadb159b15c6e4cce
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 22:08:17 +0500

    Update wall.py

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -1,15 +1,12 @@
 import textwrap
 import time
 import typing as tp
-from math import ceil
 from string import Template
-from time import sleep
 
 import pandas as pd
 from pandas import json_normalize
-from vkapi.config import VK_CONFIG
+from vkapi import config, session
 from vkapi.exceptions import APIError
-from vkapi.session import Session
 
 
 def get_posts_2500(
@@ -31,16 +28,14 @@ def get_wall_execute(
     offset: int = 0,
     count: int = 10,
     max_count: int = 2500,
-    filter: str = "owner",
+    _filter: str = "owner",
     extended: int = 0,
     fields: tp.Optional[tp.List[str]] = None,
     progress=None,
 ) -> pd.DataFrame:
     """
     Возвращает список записей со стены пользователя или сообщества.
-
     @see: https://vk.com/dev/wall.get
-
     :param owner_id: Идентификатор пользователя или сообщества, со стены которого необходимо получить записи.
     :param domain: Короткий адрес пользователя или сообщества.
     :param offset: Смещение, необходимое для выборки определенного подмножества записей.
@@ -51,39 +46,48 @@ def get_wall_execute(
     :param fields: Список дополнительных полей для профилей и сообществ, которые необходимо вернуть.
     :param progress: Callback для отображения прогресса.
     """
-    current_session = Session(base_url=VK_CONFIG["domain"])
-
-    code = """return API.wall.get({
-        "owner_id": "%s",
-        "domain": "%s",
-        "offset": %d,
-        "count": "%d",
-        "filter": "%s",
-        "extended": %d,
-        "fields": "%s",
-        "v": "%s"
-    });"""
-    answer = []
-    for i in range(ceil(count / 100)):
-        code_for_request = code % (
-            owner_id,
-            domain,
-            100 * i,
-            100 * (i + 1) if count > 100 else count,
-            filter,
-            extended,
-            fields,
-            VK_CONFIG["version"],
-        )
-        response = current_session.post(
-            url="execute",
-            data={
-                "code": code_for_request,
-                "access_token": VK_CONFIG["access_token"],
-                "v": VK_CONFIG["version"],
-            },
-        )
-        answer += response.json()["response"]["items"]
-        if i % 2 == 0:
-            sleep(1)
-    return json_normalize(answer)
+    code = f"""
+        if ({count} < 100)!
+            posts = API.wall.get(
+                !
+                    owner_id:{owner_id},
+                    domain:{domain},
+                    offset:{offset},
+                    "count":"{count}",
+                    filter:{_filter},
+                    extended:{extended},
+                    fields: {fields}
+                @
+            );
+        @
+        else !
+            posts = [];
+            for(var i = 0; i < Math.floor({count} / 100) - 1; i++) !
+                p = API.wall.get(
+                    !
+                        owner_id:{owner_id},
+                        domain:{domain},
+                        offset:{offset}+ i * 100,
+                        count: 100,
+                        filter:{_filter},
+                        extended:{extended},
+                        fields: {fields}
+                    @
+                );
+                posts.push(...p);
+            @
+        @
+        return posts;
+        """.replace(
+        "!", "{"
+    ).replace(
+        "@", "}"
+    )
+    time.sleep(2)
+    resp = session.post(
+        "execute",
+        code=code,
+        access_token=config.VK_CONFIG["access_token"],
+        v=config.VK_CONFIG["version"],
+    )
+    return json_normalize(resp.json()["response"]["items"])
