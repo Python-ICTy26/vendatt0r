2f4d4ba9864cca20756de31b3481646eb26b2d89
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 21:10:50 +0500

    Update age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -15,15 +15,15 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :return: Медианный возраст пользователя.
     """
 
-    friends_response = get_friends(user_id).items
-    friends_age = []
-    year = dt.datetime.now().year
-    for friend in friends_response:
-        try:
-            friends_age.append(year - int(friend["bdate"][5:]))
-        except:
-            pass
-    if friends_age:
-        return statistics.median(friends_age)
+    current_year = dt.datetime.now().year
+    friends = get_friends(user_id).items
+    ages = []
+    for friend in friends:
+        if (bdate := friend.get("bdate")) is not None:
+            if re.findall(r"\d[.]\d[.]\d", bdate):
+                year_of_birth = int(bdate.split(".")[-1])
+                ages.append(current_year - year_of_birth)
+    if ages:
+        return statistics.median(ages)
     else:
-        None
+        return None
