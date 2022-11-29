56c31bddeb879b4620aed5292d580438eb23d447
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 19:02:13 +0500

    Update age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -2,8 +2,7 @@ import datetime as dt
 import statistics
 import typing as tp
 
-from vkapi.friends import get_friends
-
+from homework05.vkapi.friends import get_friends
 
 def age_predict(user_id: int) -> tp.Optional[float]:
     """
@@ -14,4 +13,18 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+
+    friends_response = get_friends(user_id).items
+    friends_age = []
+    year = dt.datetime.now().year
+    for friend in friends_response:
+        try:
+            friends_age.append(year - int(friend["bdate"][5:]))
+        except:
+            pass
+    if friends_age:
+        return statistics.median(friends_age)
+    else:
+        None
+#age_predict(user_id=299615319)
+
