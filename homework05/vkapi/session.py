0a6943dd5e998b98a25a868c9af8945f223e6605
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 20:01:26 +0500

    Update session.py

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -46,10 +46,10 @@ class Session:
         if not self.session:
             self.session = requests.Session()
             retry = Retry(
-                 total=max_retries,
-                 backoff_factor=backoff_factor,
-                 raise_on_status=True,
-                 status_forcelist=[500, 502, 503, 504],
+                total=max_retries,
+                backoff_factor=backoff_factor,
+                raise_on_status=True,
+                status_forcelist=[500, 502, 503, 504],
             )
             adapter = HTTPAdapter(max_retries=retry)
             self.session.mount("https://", adapter)
