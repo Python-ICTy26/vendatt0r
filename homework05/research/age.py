d2c3a6830b276c1ffd8a2019ba233cbd25e01e20
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-11-29 22:07:01 +0500

    Update age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -9,28 +9,20 @@ from vkapi.friends import get_friends
 def age_predict(user_id: int) -> tp.Optional[float]:
     """
     Наивный прогноз возраста пользователя по возрасту его друзей.
-
     Возраст считается как медиана среди возраста всех друзей пользователя
-
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
 
-    def calculate_age(date):
-        day = int(date[0])
-        month = int(date[1])
-        year = int(date[2])
+    def calculate_age(day, month, year):
         today = dt.date.today()
-        ages = today.year - year
-        if (today.month, today.day) < (month, day):
-            ages -= 1
-        return ages
+        return today.year - year - ((today.month, today.day) < (month, day))
 
     friends = get_friends(user_id, fields=["bdate"]).items
     res = []
-    for i in friends:
-        if "bdate" in i:
-            if re.findall(r"\d[.]\d[.]\d", i["bdate"]):
-                born = i["bdate"].split(".")
-                res.append(calculate_age(born))
+    for friend in friends:
+        if "bdate" in friend:
+            if re.findall(r"\d[.]\d[.]\d", friend["bdate"]):
+                born = friend["bdate"].split(".")
+                res.append(calculate_age(int(born[0]), int(born[1]), int(born[2])))
     return statistics.median(res) if res else None
