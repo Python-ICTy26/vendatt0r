643f183bf321b247c52e21e82f9b594cae205636
vendatt0r <96778002+vendatt0r@users.noreply.github.com>
2022-12-21 17:05:05 +0500

    Update hackernews.py

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,9 +1,8 @@
 import nltk
+from bayes import NaiveBayesClassifier
 from bottle import redirect, request, route, run, template
 from db import News, fill, session
 from scraputils import get_news
-
-from bayes import NaiveBayesClassifier
 
 nltk.download("punkt")
 
